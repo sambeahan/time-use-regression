b110735b5a4037fe9a4832936071a42bef134299
sambeahan <sam.beahan@gmail.com>
2024-03-31 14:11:44 +1030

    training refactoring

--- training.py
+++ training.py
@@ -13,19 +13,19 @@ DATASET_FILE = Path(PARENT_DIR, "data", "formatted_sleep_data.csv")
 df = pd.read_csv(DATASET_FILE)
 
 
-x_vals = df[["Sleep Duration", "Physical Activity Duration", "Sedentary Time"]]
-y_labels = [
-    "Stress Level",
-    "Heart Rate",
-    "Systolic Blood Pressure",
-    "Diastolic Blood Pressure",
-    "BMI",
+X = df[["Sleep Duration", "Physical Activity Duration", "Sedentary Time"]]
+Y = df[
+    [
+        "Stress Level",
+        "Heart Rate",
+        "Systolic Blood Pressure",
+        "Diastolic Blood Pressure",
+        "BMI",
+    ]
 ]
 
-y = df[y_labels]
-
 X_train, X_test, y_train, y_test = train_test_split(
-    x_vals, y, test_size=0.3, random_state=42
+    X, Y, test_size=0.3, random_state=42
 )
 
 model = LinearRegression(fit_intercept=False)
@@ -34,31 +34,31 @@ model = model.fit(X_train.values, y_train.values)
 
 
 # print(pd.DataFrame(zip(X_train.columns, model.coef_)))
-coefficients = pd.DataFrame(model.coef_, index=y_labels, columns=x_vals.columns)
+coefficients = pd.DataFrame(model.coef_, index=Y.columns, columns=X.columns)
 # print(f"[{X_train.columns}]\n [{model.coef_.T}] + {model.intercept_}")
 print("Model coefficients:")
 print(coefficients)
 
 print("\nModel intercepts:")
 if type(model.intercept_).__module__ == np.__name__:
-    intercepts = pd.DataFrame(model.intercept_, index=y_labels)
+    intercepts = pd.DataFrame(model.intercept_, index=Y.columns)
     print(intercepts)
 else:
     print(model.intercept_)
 
 print("\nModel accuracy:")
-for i in range(len(y_labels)):
-    y_label = y_labels[i]
-    print(y_label)
+for i in range(len(Y.columns)):
+    column = Y.columns[i]
+    print(column)
     predictions = model.predict(X_test.values)[:, i : i + 1]
 
-    print("mean_squared_error : ", mean_squared_error(y_test[y_label], predictions))
-    print("mean_absolute_error : ", mean_absolute_error(y_test[y_label], predictions))
+    print("mean_squared_error : ", mean_squared_error(y_test[column], predictions))
+    print("mean_absolute_error : ", mean_absolute_error(y_test[column], predictions))
 
     print(
         "MAPE",
-        mean_absolute_error(y_test[y_label], predictions)
-        / df.loc[:, y_label].mean()
+        mean_absolute_error(y_test[column], predictions)
+        / df.loc[:, column].mean()
         * 100,
         "%\n",
     )
